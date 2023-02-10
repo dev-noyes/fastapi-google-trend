dce039acf98639e927c9c84cb9375a565dcab2f2
devbalmuda <dev.balmuda@gmail.com>
2023-02-11 01:29:41 +0900

    finish

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI, Query
 import requests
 import xml.etree.ElementTree as ET
 
+from typing import List
+from pydantic import BaseModel
+
+class TrendResult(BaseModel):
+    trends: List[str]
+
 app = FastAPI()
 
-@app.get("/trends")
+@app.get("/api/trends", response_model=TrendResult)
 def get_trends(region: str = Query("US", max_length=2)):
     url = f"https://trends.google.com/trends/trendingsearches/daily/rss?geo={region}"
     response = requests.get(url)
