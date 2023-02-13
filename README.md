9293823eee32ec512945c0a2360f80e6b3538179
devbalmuda <dev.balmuda@gmail.com>
2023-02-14 00:22:02 +0900

    add youtube api and qrcode api

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ---
-title: FastAPI
-description: A FastAPI server
+title: SideProject REST API
+description: sample rest api
 author: Dongjun Yang
 tags:
   - fastapi
@@ -9,21 +9,44 @@ tags:
   - cors
 ---
 
-# Google Trend REST API template
+# 여러 REST API 를 배포해보았습니다.
+
+## ✨ Features
+
+- CORS 대응
+- Google trend rest api 
+- Qrcode generator api
+- youtube reply api
+
+## 문서
+
+REST API 문서를 원하시는 분은 [문서](https://fastapi-google-trend.up.railway.app/redoc) 을 입력하시면 됩니다.
+
+
+## Google Trend REST API template
 
 Railway 의 fastapi 템플릿을 활용해서 만들었습니다. 
 ChatGPT 를 사용해서 google trends 를 나라별로 받아오는 api를 만들었습니다.
-문서를 원하시는 분은 [문서](https://fastapi-google-trend.up.railway.app/redoc) 을 입력하시면 됩니다.
 
-## 사용법 
+### 사용법 
 
 - 미국데이터 : https://fastapi-google-trend.up.railway.app/api/trends 
 - 한국데이터 : https://fastapi-google-trend.up.railway.app/api/trends?region=KR
 - 일본데이터 : https://fastapi-google-trend.up.railway.app/api/trends?region=JP
 
-## ✨ Features
+## Youtube reply api
 
-- FastAPI
-- Python 3
-- ChatGPT
-- CORS 대응
+유투브의 비디오 id 값을 입력하면 댓글과 대댓글을 알 수 있는 rest api.
+
+### 사용법 
+
+- 사용하기 : https://fastapi-google-trend.up.railway.app/api/youtube_comments?url=IXwzT9fcEIQ
+
+## Qrcode generator api
+
+값을 입력하면 qrcode 를 base64 로 출력해주는 rest api 입니다.
+
+### 사용법 
+
+- 사용하기 : https://fastapi-google-trend.up.railway.app/api/qrcode?name=1
+
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI, Query
-import requests
-import xml.etree.ElementTree as ET
+
 from fastapi.middleware.cors import CORSMiddleware
 
 from typing import List
 from pydantic import BaseModel
 
+import requests
+import xml.etree.ElementTree as ET
+# import re
+import qrcode
+import base64
+from io import BytesIO
+from dotenv import load_dotenv
+import os
 
-class TrendResult(BaseModel):
-    trends: List[str]
-
+load_dotenv()
+GCP_YT_APIKEY = os.environ.get('GCP_YT_APIKEY')
 
 app = FastAPI()
 
@@ -26,6 +32,10 @@ app.add_middleware(
 )
 
 
+class TrendResult(BaseModel):
+    trends: List[str]
+
+
 @app.get("/api/trends", response_model=TrendResult)
 def get_trends(region: str = Query("US", max_length=2)):
     url = f"https://trends.google.com/trends/trendingsearches/daily/rss?geo={region}"
@@ -36,3 +46,59 @@ def get_trends(region: str = Query("US", max_length=2)):
         title = item.find("title").text
         trends.append(title)
     return {"trends": trends}
+
+
+class ReplyResult(BaseModel):
+    comment: str
+    replies: List[str] | None = None
+
+
+@app.get("/api/youtube_comments", response_model=List[ReplyResult])
+async def youtube_comments(url: str = Query(..., required=True)):
+    # video_id = re.search(r"v=([^&]+)", url).group(1)
+    video_id = url
+
+    # Get the comments from the YouTube API
+    api_key = GCP_YT_APIKEY
+    api_url = f"https://www.googleapis.com/youtube/v3/commentThreads?part=snippet,replies&videoId={video_id}&textFormat=plainText&&maxResults=100&order=time&key={api_key}"
+    response = requests.get(api_url)
+    comments = response.json()
+
+    # Extract the comments and replies from the API response
+    results = []
+    for comment in comments["items"]:
+        comment_text = {}
+        reply_text = []
+        # check json key value
+        if "snippet" in comment:
+            comment_text = comment["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
+        # check json key value
+        if "replies" in comment:
+            replies = [reply["snippet"]["textDisplay"]
+                       for reply in comment["replies"]["comments"]]
+            reply_text = replies
+        results += [{"comment": comment_text, "replies": reply_text}]
+
+    return results
+
+
+class QrcodeResult(BaseModel):
+    result: str
+
+
+@app.get("/api/qrcode", response_model=QrcodeResult)
+async def qrcode_generator(name: str = Query(..., min_length=1, max_length=30)):
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=4
+    )
+    qr.add_data(name)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    buffer = BytesIO()
+    img.save(buffer, format="PNG")
+    buffer.seek(0)
+
+    return {"result": base64.b64encode(buffer.getvalue()).decode("utf-8")}
