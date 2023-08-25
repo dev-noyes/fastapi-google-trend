ee90c3da9ef980f21f73d8d32363f65e0b3fa762
devbalmuda <dev.balmuda@gmail.com>
2023-08-26 03:25:23 +0900

    add scraping

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI, Query, File, UploadFile, Request
+from fastapi import FastAPI, Query, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 from typing import Optional
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse, parse_qs
 
+import datetime
 import requests
 import xml.etree.ElementTree as ET
-import cv2
 from cv2 import dnn_superres
 from dotenv import load_dotenv
 
@@ -23,9 +25,6 @@ import urllib.request
 import json
 from random import randint
 from datetime import date
-
-import httpx
-
 
 load_dotenv()
 GCP_YT_APIKEY = os.environ.get('GCP_YT_APIKEY')
@@ -116,7 +115,7 @@ async def youtube_comments(url: str = Query(..., required=True)):
         # check json key value
         if "replies" in comment:
             replies = [reply["snippet"]["textDisplay"]
-                        for reply in comment["replies"]["comments"]]
+                       for reply in comment["replies"]["comments"]]
             reply_text = replies
         results += [{"comment": comment_text, "replies": reply_text}]
 
@@ -172,19 +171,19 @@ async def color_combination(n: int = 5):
     # 16777216 is too slow
     # colors = [f"#{format(i, '06x')}" for i in range(16777216)]
     colors = ["#000000", "#000080", "#0000ff", "#008000", "#008080", "#00ff00", "#00ffff", "#800000",
-                "#800080", "#808000", "#808080", "#c0c0c0", "#ff0000", "#ff00ff", "#ffff00", "#ffffff",
-                "#7f0000", "#7f007f", "#7f00ff", "#007f00", "#007f7f", "#00ff7f", "#00ff7f", "#7f3f00",
-                "#7f007f", "#7f7f00", "#7f7f7f", "#ff7f7f", "#ff7fff", "#ffff7f", "#4d4d4d", "#0000c6",
-                "#0000ff", "#00c600", "#00c6c6", "#00ff00", "#00ffff", "#c60000", "#c600c6", "#c6c600",
-                "#c6c6c6", "#ff0000", "#ff00ff", "#ffff00", "#ffffff", "#c10000", "#c100c1", "#c1c100",
-                "#c1c1c1", "#ff7f7f", "#ff7fff", "#ffff7f", "#7f7f7f", "#1e1e1e", "#1e1e9c", "#1e9c1e",
-                "#1e9c9c", "#9c1e1e", "#9c1e9c", "#9c9c1e", "#9c9c9c", "#4c4c4c", "#4c4cff", "#4cff4c",
-                "#4cff4c", "#4cffcf", "#4cffff", "#ff4c4c", "#ff4cff", "#ffff4c", "#ffff4c", "#ff4ccf",
-                "#ff4fff", "#ff7f00", "#ff7fff", "#ffaa00", "#ffffaa", "#cf9f9f", "#e6e6e6", "#b5b5b5",
-                "#939393", "#4c4cff", "#4cff4c", "#4cffff", "#4c4c4c", "#ff4c4c", "#ff4cff", "#ffff4c",
-                "#ffff4c", "#ff4ccf", "#ff4fff", "#ff7f00", "#ff7fff", "#ffaa00", "#ffffaa", "#cf9f9f",
-                "#e6e6e6", "#b5b5b5", "#939393", "#7f7f00", "#7f7f7f", "#7f7fff", "#7fff00", "#7fff7f"
-                ]
+              "#800080", "#808000", "#808080", "#c0c0c0", "#ff0000", "#ff00ff", "#ffff00", "#ffffff",
+              "#7f0000", "#7f007f", "#7f00ff", "#007f00", "#007f7f", "#00ff7f", "#00ff7f", "#7f3f00",
+              "#7f007f", "#7f7f00", "#7f7f7f", "#ff7f7f", "#ff7fff", "#ffff7f", "#4d4d4d", "#0000c6",
+              "#0000ff", "#00c600", "#00c6c6", "#00ff00", "#00ffff", "#c60000", "#c600c6", "#c6c600",
+              "#c6c6c6", "#ff0000", "#ff00ff", "#ffff00", "#ffffff", "#c10000", "#c100c1", "#c1c100",
+              "#c1c1c1", "#ff7f7f", "#ff7fff", "#ffff7f", "#7f7f7f", "#1e1e1e", "#1e1e9c", "#1e9c1e",
+              "#1e9c9c", "#9c1e1e", "#9c1e9c", "#9c9c1e", "#9c9c9c", "#4c4c4c", "#4c4cff", "#4cff4c",
+              "#4cff4c", "#4cffcf", "#4cffff", "#ff4c4c", "#ff4cff", "#ffff4c", "#ffff4c", "#ff4ccf",
+              "#ff4fff", "#ff7f00", "#ff7fff", "#ffaa00", "#ffffaa", "#cf9f9f", "#e6e6e6", "#b5b5b5",
+              "#939393", "#4c4cff", "#4cff4c", "#4cffff", "#4c4c4c", "#ff4c4c", "#ff4cff", "#ffff4c",
+              "#ffff4c", "#ff4ccf", "#ff4fff", "#ff7f00", "#ff7fff", "#ffaa00", "#ffffaa", "#cf9f9f",
+              "#e6e6e6", "#b5b5b5", "#939393", "#7f7f00", "#7f7f7f", "#7f7fff", "#7fff00", "#7fff7f"
+              ]
     color_combination = [choice(colors) for i in range(n)]
     return {"result": color_combination}
 
@@ -211,12 +210,14 @@ async def youtube_popular_videos(region: str = Query("US", max_length=2)):
                                     "key": api_key})
     items = response.json()["items"]
     videos = [{"link": f"https://www.youtube.com/watch?v={item['id']}",
-                "title": item["snippet"]["title"],
-                "description": item["snippet"]["description"],
-                "region": region} for item in items]
+               "title": item["snippet"]["title"],
+               "description": item["snippet"]["description"],
+               "region": region} for item in items]
     return videos
 
 # Define a Pydantic model for the YouTube video data
+
+
 class VideoData(BaseModel):
     title: str
     view_count: int
@@ -225,6 +226,8 @@ class VideoData(BaseModel):
     link: str
 
 # Define a function to retrieve the YouTube video data
+
+
 def get_video_data(topic: str, region: str) -> Optional[VideoData]:
     # Construct the URL for the video search endpoint
     search_url = 'https://www.googleapis.com/youtube/v3/search'
@@ -408,68 +411,130 @@ async def subscribers(channel_id: str):
     return {"subscriber_count": subscriber_count}
 
 
-@app.get("/api/proxy/{url:path}")
-async def reverse_proxy_get(url: str):
-    try:
-        async with httpx.AsyncClient() as client:
-            response = await client.get(url)
-            headers = response.headers
-            content = response.content
-            status_code = response.status_code
-        return content, headers, status_code
-    except Exception as e:
-        return {"result": "Error@Failed to process."}
+@app.get("/search/inflearn")
+async def search(q: str | None = None):
+    """_summary_
+    get the data from inflearn
 
+    Args:
+        q (str | None, optional): _description_. Defaults to None.
 
-@app.post("/api/proxy/{url:path}")
-async def reverse_proxy_post(url: str, request: Request):
-    try:
-        async with httpx.AsyncClient() as client:
-            response = await client.post(url, data=await request.body())
-            headers = response.headers
-            content = response.content
-            status_code = response.status_code
-        return content, headers, status_code
-    except Exception as e:
-        return {"result": "Error@Failed to process."}
+    Raises:
+        HTTPException: _description_
 
-class SuperResolutionData(BaseModel):
-    result: str
-
-@app.post("/api/super_resolution",response_model=SuperResolutionData)
-async def super_resolution(file: UploadFile = File(...)):
+    Returns:
+        array: data array
     """
-    Get the 2x upscaled image with multipart `file`.
+    if q is None:
+        raise HTTPException(status_code=404, detail="Item not found")
 
-    - `file`: file multipart for image
+    query = q
+    encoded_query = urllib.parse.quote(query)
+    url = f'https://www.inflearn.com/courses?s={encoded_query}'
 
-    Returns the retrieved item.
+    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(response.content, 'html.parser')
+    arr = []
+    for course in soup.find_all('div', {'class': 'course_card_item'}):
+        fxd_data = course.find('div', {'class': 'course-data'})['fxd-data']
+        course_data = json.loads(fxd_data)
+        course_data["course_title"] = str(
+            course_data["course_title"]).replace(",", "")
+        course_data["first_category"] = str(
+            course_data["first_category"]).replace(",", "")
+        course_data["second_category"] = str(
+            course_data["second_category"]).replace(",", "")
+        course_data["seq0_instructor_name"] = str(
+            course_data["seq0_instructor_name"]).replace(",", "")
+        course_data["skill_tag"] = str(
+            course_data["skill_tag"]).replace(",", "")
+        course_data["discount_title"] = str(
+            course_data["discount_title"]).replace(",", "", 2)
+        if (course_data["discount_id"] == None):
+            course_data["discount_id"] = "None"
+        if (course_data["discount_rate"] == None):
+            course_data["discount_rate"] = 0
+        if (course_data["installment_price"] == None):
+            course_data["installment_price"] = "None"
+        if (course_data["installment_month"] == None):
+            course_data["installment_month"] = "None"
+        arr.append(course_data)
+    return (arr)
+
+
+@app.get("/search/saramin")
+async def search(q: str | None = None, page: int = 1, limit: int = 40):
+    """_summary_
+    get the data from saramin
+
+    Args:
+        q (str | None, optional): _description_. Defaults to None.
+        page (int, optional): _description_. Defaults to 1.
+        limit (int, optional): _description_. Defaults to 40.
+
+    Raises:
+        HTTPException: _description_
+
+    Returns:
+        _type_: _description_
     """
-    try:
-        # Read image from the uploaded file
-        img_bytes = await file.read()
-        img = Image.open(io.BytesIO(img_bytes))
+    if q is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    query = q
+    encoded_query = urllib.parse.quote(query)
+    raw = requests.get(
+        f'https://www.saramin.co.kr/zf_user/search?search_area=main&search_done=y&search_optional_item=n&searchType=recently&recruitSort=reg_dt&recruitPage={page}&searchword={encoded_query}&recruitPageCount={limit}', headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(raw.text, "html.parser")
+    results = soup.select("div.item_recruit")
+    arr = []
+    for ar in results:
+        obj = {}
+        title = ar.select_one("a")['title']
+        title = title.replace(",", "")
+        obj["title"] = title
+        link = ar.select_one("a")['href']
+        obj["link"] = f"https://www.saramin.co.kr{link}"
+        company = ar.select_one(
+            'div.area_corp > strong > a').text.strip()
+        obj["company"] = company
+        enddate = ar.select_one('div.job_date > span').text.strip()
+        obj["enddate"] = enddate
 
-        # Convert PIL Image to OpenCV format
-        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        infos = ar.select('div.job_condition > span')
+        for idx in range(len(infos)):
+            if (idx == 0):
+                obj["location"] = infos[idx].text.strip()
+            if (idx == 1):
+                obj["type"] = infos[idx].text.strip()
+            if (idx == 2):
+                obj["school"] = infos[idx].text.strip()
+            if (idx == 3):
+                obj["jobtype"] = infos[idx].text.strip()
 
-        # Apply super-resolution using OpenCV's DNN module
-        # Note: this model uses the EDSR architecture and was trained on DIV2K dataset
-        model_path = "ESPCN_x2.pb"
-        sr = dnn_superres.DnnSuperResImpl_create()
-        sr.readModel(model_path)
-        sr.setModel("espcn", 2)
-        sr = sr.upsample(img)
+        tags = ar.select('div.job_sector > b > a')
+        tag_vip = ""
+        for tag in tags:
+            tag_vip += f"#{tag.text.strip()} "
+        obj["tag_vip"] = tag_vip
 
-        # Convert the super-resolved image back to PIL Image format
-        sr = cv2.cvtColor(sr, cv2.COLOR_BGR2RGB)
-        pil_img = Image.fromarray(sr)
+        tags = ar.select('div.job_sector > a')
+        tag_normal = ""
+        for tag in tags:
+            tag_normal += f"#{tag.text.strip()} "
+        obj["tag_normal"] = tag_normal
 
-        # Convert Image to base64 string
-        buffered = io.BytesIO()
-        pil_img.save(buffered, format="JPEG")
-        img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+        registerdate = ar.select_one('div.job_sector > span').text.strip()
+        obj["registerdate"] = registerdate.replace(
+            "등록일", "").replace("수정일", "")
 
-        return {"result": img_str}
-    except Exception as e:
-        return {"result": "Error@Failed to process the image."}
+        now = datetime.datetime.now()
+        nowDate = now.strftime('%Y-%m-%d')
+        obj["date"] = nowDate
+
+        try:
+            apply = ar.select_one("button.sri_btn_xs").text
+        except:
+            apply = "홈페이지지원"
+        obj["apply"] = apply
+        arr.append(obj)
+    return arr
